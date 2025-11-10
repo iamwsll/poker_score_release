81f716ad4bdfa1942fcbbcb7610a00f96ea8e6f4
iamwsll <1481121704@qq.com>
2025-11-10 17:35:28 +0800

    安卓增加状态栏隐藏

--- poker_score_frontend/src/main.ts
+++ poker_score_frontend/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
+import { Capacitor } from '@capacitor/core'
+import { StatusBar } from '@capacitor/status-bar'
 
 import App from './App.vue'
 import router from './router'
@@ -13,3 +15,18 @@ app.use(router)
 app.use(Antd)
 
 app.mount('#app')
+
+const configureAndroidStatusBar = async () => {
+  if (Capacitor.getPlatform() !== 'android') {
+    return
+  }
+
+  try {
+    await StatusBar.setOverlaysWebView({ overlay: true })
+    await StatusBar.hide()
+  } catch (error) {
+    console.warn('[StatusBar] Failed to configure Android status bar', error)
+  }
+}
+
+configureAndroidStatusBar()
