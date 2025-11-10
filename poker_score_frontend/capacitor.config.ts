18c662521c898104c82716199f6bb3591a214462
iamwsll <1481121704@qq.com>
2025-11-10 17:22:14 +0800

    新增 Capacitor 配置文件

---
+++ poker_score_frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cn.iamwsll.poker',
+  appName: 'poker_score',
+  webDir: 'dist'
+}
+
+export default config;
